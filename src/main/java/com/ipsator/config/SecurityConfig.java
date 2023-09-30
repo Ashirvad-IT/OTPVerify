46aeb444063eeef765fc768575dd3adf60947077
Ashirvad-it <ashirvad.kumar@ipsator.com>
2023-09-30 13:05:38 +0530

    Trying to implement trace id using spring cloud sleuth

--- src/main/java/com/ipsator/config/SecurityConfig.java
+++ src/main/java/com/ipsator/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ipsator.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -21,6 +22,10 @@ public class SecurityConfig {
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 	
+//	@Bean
+//    public Tracer tracer() {
+//        return new Tracer();
+//    }
 	 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
